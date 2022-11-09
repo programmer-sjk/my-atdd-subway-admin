0067c2b75ffe214ee04af7f9b4f8de80eaddc4a9
sjk5766 <wjdrnr6662@gmail.com>
2022-11-09 13:27:56 +0900

    test(acceptance): 지하철 노선 목록 조회 인수테스트 생성

--- src/test/java/nextstep/subway/station/LineAcceptanceTest.java
+++ src/test/java/nextstep/subway/station/LineAcceptanceTest.java
@@ -50,6 +50,28 @@ public class LineAcceptanceTest {
         assertThat(lineNames).containsAnyOf(expectLine);
     }
 
+    /**
+     * Given 2개의 지하철 노선을 생성하고
+     * When 지하철 노선 목록을 조회하면
+     * Then 지하철 노선 목록 조회 시 2개의 노선을 조회할 수 있다.
+     */
+    @DisplayName("지하철 노선 목록을 조회한다.")
+    @Test
+    void showLines() {
+        // given
+        String expectLine1 = "3호선";
+        String expectLine2 = "분당선";
+        createLine(expectLine1);
+        createLine(expectLine2);
+
+        // when
+        List<String> lineNames = readLines()
+                .jsonPath().getList("name", String.class);
+
+        // then
+        assertThat(lineNames).contains(expectLine1, expectLine2);
+    }
+    
     private ExtractableResponse<Response> createLine(String name) {
         Map<String, String> params = new HashMap<>();
         params.put("name", name);
