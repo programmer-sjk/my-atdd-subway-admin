7fd3e19d9092a29d2f455d76bd5dc01bf02361c3
sjk5766 <wjdrnr6662@gmail.com>
2022-11-09 12:51:11 +0900

    feat(domain): Line 도메인과 Repository 추가

--- README.md
+++ README.md
@@ -115,8 +115,8 @@ Then 해당 지하철 노선 정보는 삭제된다
 </pre>
 
 ### 기능 구현 목록
-- [ ] Line 도메인 생성
-- [ ] Line Repository 생성
+- [x] Line 도메인 생성
+- [x] Line Repository 생성
 - [ ] Line Service 생성
 - [ ] Line Controller 생성
 - [ ] 지하철 노선 생성 인수 테스트 작성
---
+++ src/main/java/nextstep/subway/domain/Line.java
@@ -0,0 +1,34 @@
+package nextstep.subway.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Line extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+
+    @Column(unique = true)
+    private String color;
+
+    public Line() {
+    }
+
+    public Line(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
