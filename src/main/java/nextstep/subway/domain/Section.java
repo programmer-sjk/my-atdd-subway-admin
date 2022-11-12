b75f025a39d439009738b64d0c92418cfe3c8b74
sjk5766 <wjdrnr6662@gmail.com>
2022-11-12 22:54:36 +0900

    feat(domain): Line 도메인 수정

--- src/main/java/nextstep/subway/domain/Section.java
+++ src/main/java/nextstep/subway/domain/Section.java
@@ -1,12 +1,17 @@
 package nextstep.subway.domain;
 
 import javax.persistence.*;
+import java.util.Arrays;
+import java.util.List;
 
 @Entity
 public class Section extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "line_id")
+    private Line line;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "up_station_id", nullable = false)
     private Station upStation;
@@ -16,5 +21,19 @@ public class Section extends BaseEntity {
     @Column(nullable = false)
     private int distance;
 
+    public Section(Station upStation, Station downStation, int distance) {
+        this.upStation = upStation;
+        this.downStation = downStation;
+        this.distance = distance;
+    }
+
     protected Section() {}
+
+    public List<Station> findStations() {
+        return Arrays.asList(upStation, downStation);
+    }
+
+    public void addLine(Line line) {
+        this.line = line;
+    }
 }
--- src/main/java/nextstep/subway/domain/Sections.java
+++ src/main/java/nextstep/subway/domain/Sections.java
@@ -1,10 +1,15 @@
 package nextstep.subway.domain;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Embeddable
 public class Sections {
+    @OneToMany(mappedBy = "line", cascade = CascadeType.ALL)
     private List<Section> sections;
 
     public Sections(List<Section> sections) {
@@ -12,4 +17,18 @@ public class Sections {
     }
 
     protected Sections() {}
+
+    public List<Station> getStations() {
+        System.out.println("???");
+        System.out.println(sections.size());
+        return sections.stream()
+                .map(Section::findStations)
+                .flatMap(Collection::stream)
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public void add(Section section) {
+        sections.add(section);
+    }
 }
